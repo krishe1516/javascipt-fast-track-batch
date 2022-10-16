ade9f7a5dd184e8a7f11ecfb978467890d5a446e
krishe1516 <sheetalshinde855@gmail.com>
2022-10-16 18:31:04 +0530

    add

---
+++ function.js
@@ -0,0 +1,49 @@
+let num1;
+let num2;
+let total;
+
+function input()
+{
+    num1=24;
+    num2=56;
+    num3=78;
+    num4=19;
+}
+ function addition()
+ {
+    total=num1+num2+num3+num4;
+    console.log('addition:-' +total)
+ }
+
+ function substraction()
+ {
+    total=num1-num2-num3-num4;
+    console.log('substraction:-'+total)
+ }
+
+ function multiplication()
+ {
+    total=num1*num2*num3*num4;
+    console.log('multiplication:-' +total)
+ }
+ function divide()
+ {
+    total=num1/num2/num3/num4;
+    console.log('divide:-' +total)
+ }
+ function all()
+ {
+    total=num1*num2/num3-num4;
+    console.log('total of all:-' +total)
+ }
+ input();
+ addition();
+ substraction();
+ multiplication();
+ divide();
+ all();
+
+
+
+
+
